eceb2a4337dfe1c2f74ce844840655df762fa4ad
ali raza <alirf50@hotmail.com>
2019-07-26 22:04:57 +0500

    Updates

---
+++ precache-manifest.062d0263ed7379fc845502f4095fa239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655bf035b5946fa066f60842b2284b9c",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "2c9c2d781577a2bb6cfd",
+    "url": "/contactmanager/static/css/main.3c4b0466.chunk.css"
+  },
+  {
+    "revision": "b3ec0da3f52040c3dc4e",
+    "url": "/contactmanager/static/js/2.c3675650.chunk.js"
+  },
+  {
+    "revision": "2c9c2d781577a2bb6cfd",
+    "url": "/contactmanager/static/js/main.6dbe54bd.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
